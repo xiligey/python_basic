ca529730aa0948f836ba19c4cbe0fd33205ffcb3
chenxilin <635943647@qq.com>
2021-08-03 15:37:19 +0800

    feat: Python更新所有包

---
+++ 实用操作/Python更新所有包.py
@@ -0,0 +1,10 @@
+from subprocess import call
+
+from pip._internal.utils.misc import get_installed_distributions
+
+if __name__ == '__main__':
+    for dist in get_installed_distributions():
+        package_name = dist.project_name
+        print('{} updating package {}'.format('--------------------\n', package_name))
+        call("pip install --upgrade " + package_name, shell=True)
+        print('√')
